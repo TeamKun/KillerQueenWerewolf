2750bc7237bc50263ea705f99780bde293e99518
Dedenneee <st.na.fm@gmail.com>
2021-05-21 03:10:23 +0900

    何にも進んでない。全てがダメな日。そんなコミット。

--- src/main/java/net/kunmc/lab/killerqueen/KillerQueen.java
+++ src/main/java/net/kunmc/lab/killerqueen/KillerQueen.java
@@ -1,5 +1,6 @@
 package net.kunmc.lab.killerqueen;
 
+import net.kunmc.lab.killerqueen.listener.CommandListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class KillerQueen extends JavaPlugin {
@@ -7,11 +8,13 @@ public final class KillerQueen extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        getServer().getPluginManager().registerEvents(new CommandListener(), this);
+        getLogger().info("キラークイーンが有効になりました");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        getLogger().info("キラークイーンが有効になりました");
     }
 }
---
+++ src/main/java/net/kunmc/lab/killerqueen/listener/CommandListener.java
@@ -0,0 +1,58 @@
+package net.kunmc.lab.killerqueen.listener;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.Listener;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class CommandListener implements Listener {
+
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+        if (!cmd.getName().equalsIgnoreCase("stand")) {
+            return false;
+        }
+        if (args.length < 1) {
+            sender.sendMessage("引数が足りません");
+            return false;
+        } else if (args.length > 3) {
+            sender.sendMessage("引数が多すぎます");
+            return false;
+        }
+
+        if (!(args[0].equalsIgnoreCase("killerqueen") || args[0].equalsIgnoreCase("crazydiamond"))) {
+            sender.sendMessage("第1引数にはスタンド名を入力してください");
+            return true;
+        }
+
+        if (!(args[1].equalsIgnoreCase("add") || args[1].equalsIgnoreCase("remove"))) {
+            sender.sendMessage("第2引数にはadd/removeを入力してください");
+            return true;
+        }
+        if (args[2].startsWith("@")) {
+            if(Bukkit.selectEntities(sender, args[2]) != null){
+
+            }
+        } else {
+            if (Bukkit.getPlayer(args[2]) != null) {
+
+            }
+        }
+
+        return false;
+    }
+
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        switch (args.length) {
+            case 1:
+                return Stream.of("setup", "start")
+                        .filter(e -> e.startsWith(args[0]))
+                        .collect(Collectors.toList());
+        }
+        return Collections.emptyList();
+    }
+}
